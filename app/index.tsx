0750798d904c4b3d61d30acb0396682c2332a6e5
amanhaidry <amanhaidry9480@gmail.com>
2025-07-30 22:21:51 +0530

    chore(setup): configure project styling system with tailwind and nativewind - Add tailwindcss and nativewind configuration - Setup babel and metro configs for styling support - Add global CSS configuration - Install required dependencies

--- app/index.tsx
+++ app/index.tsx
@@ -2,14 +2,10 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View className="flex-1 bg-gray-50">
+      <Text className="font-bold">Welcome.</Text>
+        <Text>Hello</Text>
+
     </View>
   );
 }
